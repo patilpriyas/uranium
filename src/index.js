682275bd8867159b0c5519870cd3a0915a1b27e1
“patilpriyas” <patilpriya141994@gmail.com>
2022-04-20 11:33:31 +0530

    assignment

--- src/index.js
+++ src/index.js
@@ -8,21 +8,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+
+mongoose.connect("mongodb+srv://priya:priya1122@cluster0.zscq3.mongodb.net/myDatabase",
+{
+    maxPoolSize: 50, 
+    wtimeoutMS: 2500,
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+.then(  () => console.log("MongoDB is Connected"))
+.catch(err => console.log(err))
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        let dateTime = require('node-datetime');
+        let dt = dateTime.create();
+        let formatted = dt.format('Y-m-d H:M:S');
+        console.log(formatted + " " + req.ip + " " + req.originalUrl);
         next();
   }
   );
 
 app.use('/', route);
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
