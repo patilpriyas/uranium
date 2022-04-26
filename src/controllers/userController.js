6ea108ac438292a35478a976412a0893d446d8bb
“patilpriyas” <patilpriya141994@gmail.com>
2022-04-26 13:36:12 +0530

    done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,28 @@
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
+
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
+
+const createNewBook = async function(req, res){
+    let bookData = req.body
+    let savedBookData = await UserModel.create(bookData)
+    res.send({msg: savedBookData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const getAllBookList = async function(req,res){
+    let allBooks = req.body
+    res.send({msg: allBooks})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
+module.exports.createNewBook= createNewBook
+module.exports.getAllBookList = getAllBookList
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,16 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser)
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
+
+router.post("/createBook", UserController.createNewBook)
+
+router.post("/getAllBookList", UserController.getAllBookList)
 
 module.exports = router;
+
+//Create a bookSchema with bookName, authorName, category and year . 
+//Create same 2 api's for books i.e. : 1 api to create a new book 
+//and another api to get the list of all books.
